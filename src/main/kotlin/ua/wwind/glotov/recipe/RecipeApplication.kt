bcffeb6131eefc311066ba5c8ef9b58a158a251c
Oleksandr Glotov <alexander.v.glotov@gmail.com>
2022-12-01 01:52:27 +0200

    Initial commit

---
+++ src/main/kotlin/ua/wwind/glotov/recipe/RecipeApplication.kt
@@ -0,0 +1,11 @@
+package ua.wwind.glotov.recipe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecipeApplication
+
+fun main(args: Array<String>) {
+    runApplication<RecipeApplication>(*args)
+}
