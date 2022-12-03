0cd77ecb274f35d366f76539159f46d2a6850dab
Oleksandr Glotov <alexander.v.glotov@gmail.com>
2022-12-03 15:50:59 +0200

    Database initialization with categories and UoM

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (name) VALUES ('American');
+INSERT INTO category (name) VALUES ('Italian');
+INSERT INTO category (name) VALUES ('Mexican');
+INSERT INTO category (name) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (uom) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (uom) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (uom) VALUES ('Cup');
+INSERT INTO unit_of_measure (uom) VALUES ('Pinch');
+INSERT INTO unit_of_measure (uom) VALUES ('Ounce');
